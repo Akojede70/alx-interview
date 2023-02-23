1921a6fe116a08f8d13156e14000926b3a880638
Akojede70 <peter.oluwamayowa.akojede@gmail.com>
2023-02-23 08:38:19 +0100

    making

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" Making change """
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
 
 
 def makeChange(coins, total):
-    """ Returns fewest number of coins needed to meet total
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
     """
-
     if total <= 0:
         return 0
-    else:
-        from math import trunc
 
-        coins = sorted(coins, reverse=True)
-        coin_dict = {}
-        while total is not None:
-            for c in coins:
-                if total % c == 0:
-                    coin_dict[c] = total / c
-                    return(int(sum(coin_dict.values())))
-                else:
-                    coin_dict[c] = trunc(total / float(c))
-                    total -= (c * coin_dict[c])
-            return -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
